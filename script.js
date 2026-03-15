ccca5e0251845fd0f868e0675232480859d6a28e
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-03-15 19:41:51 -0300

    atualização

--- script.js
+++ script.js
@@ -3,12 +3,29 @@ document.querySelectorAll('.item').forEach(item => {
     item.addEventListener('dragend', dragEnd); 
 });
 
-//Functions Item
-function dragStar() {
-    
-}
-function dragEnd() {
+document.querySelectorAll('.area').forEach(area => {
+    // vai ser rodada sempre que estiver arrastando um item e esse item passar por cima da area em que adicionou o evento
+    area.addEventListener('dragover', dragOver);
 
+    area.addEventListener('dragleave', dragLeave);
+    area.addEventListener('drop', drop);
+});
+
+//Functions Item
+function dragStar(e) {
+    e.currentTarget.classList.add('dragging');
+}
+function dragEnd(e) {
+    e.currentTarget.classList.remove('dragging');
 }
 
 //Functions Area
+function dragOver(e) {
+    console.log("Passou por cima!");
+}
+function dragLeave() {
+
+}
+function drop() {
+
+}
